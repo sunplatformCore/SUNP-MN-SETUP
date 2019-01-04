61b8311ad6ecd5ef4c16ca147b98a26582e57771
sampathkumar <sampathkumar@digital-nirvana.com>
2019-01-04 17:01:49 +0530

    updated MN setup

--- sunp-mn-install.sh
+++ sunp-mn-install.sh
@@ -1,143 +1,169 @@
 #!/bin/bash
 
-# Variables
-RED_TEXT=`tput setaf 1`
-GREEN_TEXT=`tput setaf 2`
-RESET_TEXT=`tput sgr0`
-REQUIRED_UBUNTU_VERSION="16.04"
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=6
+BACKTITLE="sunplatform Masternode Setup Wizard"
+TITLE="sunplatform VPS Setup"
+MENU="Choose one of the following options:"
 
-# clear screen
+OPTIONS=(1 "Install New VPS Server"
+         2 "Update to new version VPS Server"
+         3 "Start sunplatform Masternode"
+	 4 "Stop sunplatform Masternode"
+	 5 "sunplatform Server Status"
+	 6 "Rebuild sunplatform Masternode Index")
+
+
+CHOICE=$(whiptail --clear\
+		--backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
 clear
-
-# Required Ubuntu Version check
-echo -n 'Checking Ubuntu Linux Version...'
-if [[ `lsb_release -rs` == $REQUIRED_UBUNTU_VERSION ]]
-then
-	echo "${GREEN_TEXT} 16.04 OK ${RESET_TEXT}"; echo ""
+case $CHOICE in
+        1)
+            echo Starting the install process.
+echo Checking and installing VPS server prerequisites. Please wait.
+echo -e "Checking if swap space is needed."
+PHYMEM=$(free -g|awk '/^Mem:/{print $2}')
+SWAP=$(swapon -s)
+if [[ "$PHYMEM" -lt "2" && -z "$SWAP" ]];
+  then
+    echo -e "${GREEN}Server is running with less than 2G of RAM, creating 2G swap file.${NC}"
+    dd if=/dev/zero of=/swapfile bs=1024 count=2M
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon -a /swapfile
 else
-	echo "${RED_TEXT} Your Server is not running Ubuntu $REQUIRED_UBUNTU_VERSION, please upgrade to Ubuntu $REQUIRED_UBUNTU_VERSION ! The script will be terminated... ${RESET_TEXT}"; echo ""
-	exit
+  echo -e "${GREEN}The server running with at least 2G of RAM, or SWAP exists.${NC}"
+fi
+if [[ $(lsb_release -d) != *16.04* ]]; then
+  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+  exit 1
 fi
 
-read -e -p "Is your VPS Provider allowing to create SWAP file? If not sure hit enter! [Y/n] : " swapallowed
-if [[ ("$swapallowed" == "y" || "$swapallowed" == "Y") ]]; then
-  echo "Creating 2GB SWAP file..."
-  sudo touch /mnt/swap.img
-  sudo chmod 0600 /mnt/swap.img
-  dd if=/dev/zero of=/mnt/swap.img bs=1024k count=2000
-  sudo mkswap /mnt/swap.img
-  sudo swapon /mnt/swap.img
-  sudo echo "/mnt/swap.img none swap sw 0 0" >> /etc/fstab
-  echo ""
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${RED}$0 must be run as root.${NC}"
+   exit 1
 fi
-
-echo "Make sure you double check before pressing enter! One chance at this only!"; echo ""
-
-# Ask for important Data for configuring Masternode
-read -e -p "Server IP Address : " ip
-read -e -p "Masternode Private Key (Generate via local Wallet Debug Console -> masternode genkey) : " key
-read -e -p "Install Fail2ban? [Y/n] : " install_fail2ban
-read -e -p "Install UFW and configure ports? [Y/n] : " UFW
-
-# Update, upgrade Ubuntu and install required packages
 clear
-cd ~
-echo "Updating system and installing required packages..."
-sudo apt-get -y update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
-sudo apt-get -y autoremove
-sudo apt-get install curl wget nano htop -y
-sudo apt-get install automake build-essential libtool autotools-dev autoconf pkg-config libssl-dev -y
-sudo apt-get install libboost-all-dev git npm nodejs nodejs-legacy libminiupnpc-dev redis-server -y
+sudo apt update
+sudo apt-get -y upgrade
+sudo apt-get install git -y
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils -y
+sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
+sudo apt-get install libboost-all-dev -y
 sudo apt-get install software-properties-common -y
-sudo apt-get install libevent-dev -y
-add-apt-repository ppa:bitcoin/bitcoin -y
-apt-get update -y
-apt-get install libdb4.8-dev libdb4.8++-dev -y
-source ~/.profile
-apt-get -y install aptitude
-echo ""
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+sudo apt-get install libminiupnpc-dev -y
+sudo apt-get install libzmq3-dev -y
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y
+sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler -y
+clear
+echo VPS Server prerequisites installed.
 
-# Download Wallet files and copying to /usr/local/bin
-echo "Downloading Wallet files and extracting..."
-mkdir ~/sunplatform/
+
+echo Configuring server firewall.
+sudo apt-get install -y ufw
+sudo ufw allow 62891
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw logging on
+echo "y" | sudo ufw enable
+sudo ufw status
+echo Server firewall configuration completed.
+
+echo Downloading AquilaX install files.
 wget https://github.com/sunplatformCore/sunplatform/releases/download/v1.0.0/sunplatformd
-wget https://github.com/sunplatformCore/sunplatform/releases/download/v1.0.0/sunplatform-cli
 wget https://github.com/sunplatformCore/sunplatform/releases/download/v1.0.0/sunplatform-tx
+wget https://github.com/sunplatformCore/sunplatform/releases/download/v1.0.0/sunplatform-cli
+echo Download complete.
 
+echo Installing sunplatform.
+chmod 775 ./sunplatformd
+chmod 775 ./sunplatform-cli
+echo sunplatform install complete. 
+clear
 
-cd ~/sunplatform
-cp -f sunplatformd /usr/local/bin
-cp -f sunplatform-cli /usr/local/bin
-cp -f sunplatform-tx /usr/local/bin
-echo ""
+echo Now ready to setup sunplatform configuration file.
 
-# Delete Files
-echo "Deleting unnecessary files..."
-rm -rf ~/sunplatform/
-echo ""
+RPCUSER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+RPCPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+EXTIP=`curl -s4 icanhazip.com`
+echo Please input your private key.
+read GENKEY
 
-# Crontab entry to start sunplatform after server reboot
-echo "Creating crontab entry..."
-(crontab -l ; echo "@reboot sleep 15 && /usr/local/bin/sunplatformd -daemon -shrinkdebugfile")| crontab -
-echo ""
+mkdir -p /root/.sunplatform && touch /root/.sunplatform/sunplatform.conf
 
-# Installing, configuring Firewall and Fail2Ban
-if [[ ("$install_fail2ban" == "y" || "$install_fail2ban" == "Y" || "$install_fail2ban" == "") ]]; then
-  echo "Installing and starting Fail2Ban..."
-  cd ~
-  sudo aptitude -y install fail2ban
-  sudo service fail2ban restart
-  echo ""
-fi
-if [[ ("$UFW" == "y" || "$UFW" == "Y" || "$UFW" == "") ]]; then
-  echo "Installing and starting Firewall..."
-  sudo apt-get install ufw
-  sudo ufw default deny incoming
-  sudo ufw default allow outgoing
-  sudo ufw logging on
-  sudo ufw allow ssh/tcp
-  sudo ufw limit ssh/tcp
-  sudo ufw allow 8329/tcp
-  sudo ufw allow 8330/tcp
-  sudo echo "y" | sudo ufw enable
-  echo ""
-fi
-
-# Generating Random Passwords, creating sunplatform.conf file
-echo "Creating sunplatform.conf file..."
-password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
-password2=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
-
-sudo mkdir ~/.sunplatform
-sudo touch ~/.sunplatform/sunplatform.conf
-echo 'rpcuser='$password'
-rpcpassword='$password2'
+cat << EOF > /root/.sunplatform/sunplatform.conf
+rpcuser=$RPCUSER
+rpcpassword=$RPCPASSWORD
 rpcallowip=127.0.0.1
-rpcport=62891
-masternodeaddr=127.0.0.1:62890
-externalip='$ip'
-listen=1
 server=1
+listen=1
 daemon=1
-maxconnections=128
+staking=1
+rpcallowip=127.0.0.1
+rpcport=62890
+port=62891
+logtimestamps=1
+maxconnections=256
 masternode=1
-masternodeprivkey='$key'
-addnode=202.182.109.148
-' | sudo -E tee ~/.sunplatform/sunplatform.conf >/dev/null 2>&1
-    sudo chmod 0600 ~/.sunplatform/sunplatform.conf
-echo ""
+externalip=$EXTIP
+masternodeprivkey=$GENKEY
 
-# Starting sunplatform daemon
-echo "Starting Masternode..."
-sunplatformd -daemon
-echo ""
-echo "${RED_TEXT}Please start you masternode via local desktop wallet debug console -> masternode start-alias YOURMASTERNODEALIAS !${RESET_TEXT}"; echo ""
-read -p "After starting your Masternode, press any key to continue... " -n1 -s
-echo ""
+EOF
+clear
+./sunplatformd -daemon
+./sunplatform-cli stop
+./sunplatformd -daemon
+clear
+echo sunplatform configuration file created successfully. 
+echo sunplatform Server Started Successfully using the command ./sunplatformd -daemon
+echo If you get a message asking to rebuild the database, please hit Ctr + C and run ./sunplatformd -daemon -reindex
+echo If you still have further issues please reach out to support in our Discord channel. 
+echo Please use the following Private Key when setting up your wallet: $GENKEY
+            ;;
+	    
+    
+        2)
+sudo ./sunplatform-cli -daemon stop
+echo "! Stopping sunplatform Daemon !"
 
-# Show Version and Masternde Info
-echo "Getting Masternode Output (60 sec waiting time...)"
-sleep 60
-sunplatform-cli getinfo
-rm -f ~/sunp-mn-install.sh
+echo Configuring server firewall.
+sudo apt-get install -y ufw
+sudo ufw allow 62891
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw logging on
+echo "y" | sudo ufw enable
+sudo ufw status
+echo Server firewall configuration completed.
+
+
+
+./sunplatformd -daemon
+echo sunplatform install complete. 
+
+
+            ;;
+        3)
+            ./sunplatformd -daemon
+		echo "If you get a message asking to rebuild the database, please hit Ctr + C and rebuild sunplatform Index. (Option 6)"
+            ;;
+	4)
+            ./sunplatform-cli stop
+            ;;
+	5)
+	    ./sunplatform-cli getinfo
+	    ;;
+        6)
+	     ./sunplatformd -daemon -reindex
+            ;;
+esac
